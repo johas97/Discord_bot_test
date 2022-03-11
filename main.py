9252d986402bfd0ee3e92245eea8fccecd9fbcaf
johas97 <92154809+johas97@users.noreply.github.com>
2022-03-11 16:14:29 +0000

    Fixed some buggs

--- main.py
+++ main.py
@@ -19,19 +19,35 @@ async def on_message(message):
   if message.author == client.user: 
     return
   messg = message.content
-  #Meme actions
+ 
   
+  #Meme actions
   if messg.startswith('summon' or 'Summon'):
     json_data = meme.get_meme()
     await message.channel.send(json_data['title'])
     await message.channel.send(json_data['preview'][-1])
-  #Tagging actions
-
   
+    
+    #Tagging actions
   if  messg.startswith('tag' or 'Tag'): 
     messg_arr = messg.split(' ', 3) 
     emoji_id = messg_arr[3]
     user_id = messg_arr[1]
+    reactions.add_to_taglist(user_id, emoji_id)
+
+  print(reactions.is_tagged(message.author))
+  if reactions.is_tagged(message.author):
+    reactions.execute_reactions(message.author)
+    
+
+    
+    
+    
+
+    
+    
+
+    
 
     
 
--- reactions.py
+++ reactions.py
@@ -4,12 +4,44 @@ import requests
 import json
 from pprint import pprint 
 
-suslist = []
-def add_to_suslist(user):
-  suslist.push(user)
+taglist = []
 
-def in_suslist(user):
-  return user in suslist
+class PersonTag: 
+  def __init__(self, user_id, emoji_id):
+    self.user_id = user_id
+    self.emoji_id = emoji_id
+    
+def add_to_taglist(user_id, emoji_id):
+  
+  user = PersonTag(user_id, emoji_id)
+  taglist.append(user)
+
+# 1) Fixa is tagged funktionen
+def is_tagged(user_id):
+  for user in taglist:
+    print(user.user_id)
+    print(user_id)
+    if str(user.user_id) == str(user_id):
+       return True
+       
+  return False
+# 2) Jobba med att utföra reactions! 
+def execute_reactions(user_id):
+  actions_to_execute = []
+  for user in taglist: 
+    if user.user_id == user_id: 
+      actions_to_execute.append(user)
+
+  
+
+  
+      
+    
+  
+  
+       
+    
+  
 
 
   
